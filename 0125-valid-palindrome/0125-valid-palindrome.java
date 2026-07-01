018954f91b94d41b3aa306a0aecdbfc4d9a61167
Yesh Bind <yeshkb10@gmail.com>
2026-07-01 11:06:15 +0530

    Time: 14 ms (32.29%), Space: 46.2 MB (32.31%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        s = s.replaceAll("[^a-zA-Z0-9]"  , "").toLowerCase();
+        
+        int n= s.length();
+        int l = 0 ;
+        int r =n-1;
+
+        while(l<r){
+
+            if(s.charAt(l)!= s.charAt(r)){
+            return false;
+            }
+
+            l++;
+            r--;
+
+        }
+
+        return true;
+        
+    }
+}
