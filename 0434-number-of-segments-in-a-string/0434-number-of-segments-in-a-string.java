73da1f114e51754da4eda8afb00d75eda2b3c0a6
Yesh Bind <yeshkb10@gmail.com>
2026-07-07 10:47:23 +0530

    Time: 0 ms (100%), Space: 42.8 MB (14.12%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i = 0 ; i<n ; i++){
             if(s.charAt(i) == ' ' || s.charAt(i) == '\n' ||  s.charAt(i) == '\t'){
                 flag = true;
-
+    
             }
 
             else {
