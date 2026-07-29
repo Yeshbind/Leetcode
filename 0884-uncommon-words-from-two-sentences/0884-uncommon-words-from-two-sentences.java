911f6cd7826bcefea235ff4f5aa2667ef9cc0d17
Yesh Bind <yeshkb10@gmail.com>
2026-07-29 08:34:11 +0530

    Time: 2 ms (99.56%), Space: 43.5 MB (41.53%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String[] uncommonFromSentences(String s1, String s2) {
+
+        String[] arr1 = s1.split(" ");
+        String[] arr2 = s2.split(" ");
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String s : arr1 ){
+            map.put(s , map.getOrDefault(s, 0) +1 );      
+              }
+
+        for(String s : arr2 ){
+            map.put(s , map.getOrDefault(s, 0) +1 );      
+              }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for(String key : map.keySet()){
+            if(map.get(key) == 1 ){
+                list.add(key);
+            }
+        }
+
+        return list.toArray(new String[0]);
+    }
+}
