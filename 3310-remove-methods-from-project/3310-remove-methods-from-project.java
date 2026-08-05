c04873592091d6d053840cc524f18444f02545e1
Yesh Bind <yeshkb10@gmail.com>
2026-08-05 21:35:02 +0530

    Time: 81 ms (47.12%), Space: 302.8 MB (5.77%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        ArrayList<Integer>[] graph = new ArrayList[n];
+
+        for(int i = 0; i < n; i++)
+            graph[i] = new ArrayList<>();
+
+        for(int[] edge : invocations)
+            graph[edge[0]].add(edge[1]);
+
+        boolean[] suspicious = new boolean[n];
+
+        dfs(k, graph, suspicious);
+
+        for(int[] edge : invocations){
+            int u = edge[0];
+            int v = edge[1];
+
+            if(!suspicious[u] && suspicious[v]){
+                List<Integer> all = new ArrayList<>();
+                for(int i = 0; i < n; i++)
+                    all.add(i);
+                return all;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            if(!suspicious[i])
+                ans.add(i);
+        }
+
+        return ans;
+    }
+
+    private void dfs(int node, ArrayList<Integer>[] graph, boolean[] vis){
+        if(vis[node]) return;
+
+        vis[node] = true;
+
+        for(int next : graph[node])
+            dfs(next, graph, vis);
+    }
+}
